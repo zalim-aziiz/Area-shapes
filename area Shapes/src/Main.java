805d9ac29aa3c51e5416d7b51c02ff85d7df1c8d
zalim-aziiz <125902570+zalim-aziiz@users.noreply.github.com>
2023-02-20 19:27:08 +0300

    Add files via upload
    My first commit

---
+++ area Shapes/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        //Areacylinder=====================
+   //Area of a cylinder
+        double radius, hieght, area;
+
+    }
+}
